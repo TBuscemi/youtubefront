03d41106c368f9292d32d00dc2bea56e6218429a
TBuscemi <thomasbuscemi124@gmail.com>
2021-10-06 13:20:56 -0400

    building it in app.jsx and will be moving it to seperate folders but wanted to get it working so far.i got the api work as well as the search working with the api. still need to workon on the displaying the info and related videos part .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react';
+import axios from 'axios'
+import './App.css';
+// import VideoCard from './components/VideoCard'
+import { Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/button';
 
 
 
-export default class App extends Component {
+class App extends Component{
   constructor(props){
     super(props);
-    this.state = {
-    relatedVideos: [],
-    comments: {}
-  }            
-}
-  
-  
-  componentWillUnmount(){
-    
+    this.state={
+      searchFor: "",
+      video_id: "",
+      url_head: "https://www.youtube.com/embed/",
+      video_url: "https://www.youtube.com/embed/pBI3lc18k8Q",
+      related_videos:[]
+    };
+    this.searchClicked = this.searchClicked.bind(this);
   }
-  
-  
-  
-  
-  render() {
+
+  async searchClicked(){
+    let searchString = this.state.searchFor.split(' ').join('%');
+    let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q="+searchString+"&key=AIzaSyCkWL84dG2bkXEffwiI8MGLOJHzYWSSdWI");
+    this.setState({video_id: response.data.items[0].id.videoId})
+    this.setState({video_url: this.state.url_head + this.state.video_id})
+  }
+
+
+  render(){
     return (
-      <div>
-        
+      <div className="container text-center appstyle">
+        <br/>
+        <br/>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-10 text-center">
+            <input onChange={(event) => this.setState({searchFor: event.target.value})} value={this.state.searchFor} name="searchFor" id="searchFor" type="text" />
+            <Button onClick={this.searchClicked}>Search</Button> 
+            <br/>
+            <br/>
+            <br/>
+          </div>
+          <div className="col-1"> </div>
+        </div>
+        <div className="row">
+          <div className="col-1"> </div>
+          <div className="col-10 text-center"> 
+            <iframe width="560" height="315" src= {this.state.video_url} 
+            title="YouTube video player" frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen />
+          </div>
+          <div className="col-1"> </div>
+        </div>
+        <div className="row">
+          <div className="col-1"> </div>
+          <div className="col-10"> 
+            <table className="videoCards" align="center">
+            </table>
+          </div>
+          <div className="col-1"> </div>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+
+
+export default App;
